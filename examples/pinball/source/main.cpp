2a6d8d6ca717c0d58f9de182f31b7e7f111f4c6a
Will Smith <minkcv@gmail.com>
2019-01-20 11:05:56 -0700

    setup box2d as library in the same way as libcross2d

--- examples/pinball/source/main.cpp
+++ examples/pinball/source/main.cpp
@@ -1,4 +1,4 @@
-#include <Box2D/Box2D.h>
+#include "Box2D/Box2D.h"
 #include <random>
 #include "cross2d/c2d.h"
 
@@ -16,6 +16,34 @@ int main(int argc, char **argv) {
 
     // Test box2d
     b2Vec2 gravity(-10.0f, 0.0f);
+    b2World world(gravity);
+
+    b2BodyDef groundBodyDef;
+    groundBodyDef.position.Set(0.0f, -10.0f);
+
+    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+
+    b2PolygonShape groundBox;
+    groundBox.SetAsBox(50.0f, 10.0f);
+    groundBody->CreateFixture(&groundBox, 0.0f);
+
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_dynamicBody;
+    bodyDef.position.Set(0.0f, 4.0f);
+    b2Body* body = world.CreateBody(&bodyDef);
+
+    b2PolygonShape dynamicBox;
+    dynamicBox.SetAsBox(1.0f, 1.0f);
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.shape = &dynamicBox;
+    fixtureDef.density = 1.0f;
+    fixtureDef.friction = 0.3f;
+    body->CreateFixture(&fixtureDef);
+
+    float32 timeStep = 1.0f / 60.0f;
+    int32 velocityIterations = 6;
+    int32 positionIterations = 2;
 
     // main loop
     while (true) {
@@ -31,6 +59,11 @@ int main(int argc, char **argv) {
                 break;
             }
         }
+
+	world.Step(timeStep, velocityIterations, positionIterations);
+	b2Vec2 position = body->GetPosition();
+	float32 angle = body->GetAngle();
+	printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
 
         renderer->flip();
     }
