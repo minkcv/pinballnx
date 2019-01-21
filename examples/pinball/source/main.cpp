69f722c60b0b3bd6a90be3e4d2fed1244ee59328
Will Smith <minkcv@gmail.com>
2019-01-21 14:41:37 -0700

    add demo circle

--- examples/pinball/source/main.cpp
+++ examples/pinball/source/main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char **argv) {
 
     b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(13.5f, 4.0f);
+    bodyDef.position.Set(13.5f, 18.0f);
     b2Body* body = world.CreateBody(&bodyDef);
 
     b2PolygonShape dynamicBox;
@@ -54,6 +54,20 @@ int main(int argc, char **argv) {
     fixtureDef.density = 1.0f;
     fixtureDef.friction = 0.3f;
     body->CreateFixture(&fixtureDef);
+
+    b2CircleShape circle;
+    circle.m_radius = 1.0f;
+
+    b2BodyDef circleBodyDef;
+    circleBodyDef.type = b2_dynamicBody;
+    circleBodyDef.position.Set(18.0f, 1.0f);
+    b2Body* circleBody = world.CreateBody(&circleBodyDef);
+    b2FixtureDef circleFixtureDef;
+    circleFixtureDef.shape = &circle;
+    circleFixtureDef.density = 1.0f;
+    circleFixtureDef.friction = 0.3f;
+    circleBody->CreateFixture(&circleFixtureDef);
+
 
     float32 timeStep = 1.0f / 60.0f;
     int32 velocityIterations = 6;
@@ -72,6 +86,9 @@ int main(int argc, char **argv) {
     renderer->add(box);
 
     Vector2<float> vTopLeft(-1.0f, -1.0f);
+
+    CircleShape* shape3 = new CircleShape(10);
+    renderer->add(shape3);
 
     // main loop
     while (true) {
@@ -99,6 +116,12 @@ int main(int argc, char **argv) {
         float boxY = (position.y - transform.y) * 10;
         box->setPosition(boxX, boxY);
         box->setRotation(angle * 180 / M_PI); // cross2d uses degrees.
+
+        position = circleBody->GetPosition();
+        angle = circleBody->GetAngle();
+        transform = rotateVector(vTopLeft, -angle);
+        shape3->setPosition((position.x + transform.x) * 10, (position.y - transform.y) * 10);
+        shape3->setRotation(angle * 180 / M_PI);
 
         renderer->flip();
     }
