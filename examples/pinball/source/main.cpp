fb555f9e9f399c8335acd7dd88e0d25c34dad96e
Will Smith <minkcv@gmail.com>
2019-01-21 10:50:41 -0700

    graphics for box2d demo code

--- examples/pinball/source/main.cpp
+++ examples/pinball/source/main.cpp
@@ -1,9 +1,15 @@
 #include "Box2D/Box2D.h"
-#include <random>
 #include "cross2d/c2d.h"
+#include <math.h>
 
 using namespace c2d;
 
+Vector2<float> rotateVector(Vector2<float> in, double radians) {
+    float x = cos(radians) * in.x + sin(radians) * in.y;
+    float y = sin(radians) * in.x - cos(radians) * in.y;
+    Vector2<float> result(x, y);
+    return result;
+}
 
 int main(int argc, char **argv) {
 
@@ -15,21 +21,24 @@ int main(int argc, char **argv) {
     renderer->getInput()->setRepeatDelay(1000);
 
     // Test box2d
-    b2Vec2 gravity(-10.0f, 0.0f);
+    b2Vec2 gravity(0.0f, 10.0f);
     b2World world(gravity);
 
     b2BodyDef groundBodyDef;
-    groundBodyDef.position.Set(0.0f, -10.0f);
-
+    groundBodyDef.position.Set(8.0f, 20.0f);
     b2Body* groundBody = world.CreateBody(&groundBodyDef);
 
+    groundBodyDef.position.Set(18.0f, 40.0f);
+    b2Body* groundBody2 = world.CreateBody(&groundBodyDef);
+
     b2PolygonShape groundBox;
-    groundBox.SetAsBox(50.0f, 10.0f);
+    groundBox.SetAsBox(5.0f, 1.0f);
     groundBody->CreateFixture(&groundBox, 0.0f);
+    groundBody2->CreateFixture(&groundBox, 0.0f);
 
     b2BodyDef bodyDef;
     bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f, 4.0f);
+    bodyDef.position.Set(13.5f, 4.0f);
     b2Body* body = world.CreateBody(&bodyDef);
 
     b2PolygonShape dynamicBox;
@@ -44,6 +53,20 @@ int main(int argc, char **argv) {
     float32 timeStep = 1.0f / 60.0f;
     int32 velocityIterations = 6;
     int32 positionIterations = 2;
+
+    auto r = FloatRect((8 - 5) * 10, (20 - 1) * 10, 100, 20);
+    Shape* shape = new C2DRectangle(r);
+    renderer->add(shape);
+
+    auto r3 = FloatRect((18 - 5) * 10, (40 - 1) * 10, 100, 20);
+    Shape* shape2 = new C2DRectangle(r3);
+    renderer->add(shape2);
+
+    auto r2 = FloatRect((13.1 - 1) * 10, (4 - 1) * 10, 20, 20);
+    Shape* box = new C2DRectangle(r2);
+    renderer->add(box);
+
+    Vector2<float> vTopLeft(-1.0f, -1.0f);
 
     // main loop
     while (true) {
@@ -60,10 +83,17 @@ int main(int argc, char **argv) {
             }
         }
 
-	world.Step(timeStep, velocityIterations, positionIterations);
-	b2Vec2 position = body->GetPosition();
-	float32 angle = body->GetAngle();
-	printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+        world.Step(timeStep, velocityIterations, positionIterations);
+        b2Vec2 position = body->GetPosition();
+        float32 angle = body->GetAngle();
+        //printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+
+        Vector2<float> transform = rotateVector(vTopLeft, -angle);
+        //printf("%4.2f %4.2f\n", transform.x, transform.y);
+        float boxX = (position.x + transform.x) * 10;
+        float boxY = (position.y - transform.y) * 10;
+        box->setPosition(boxX, boxY);
+        box->setRotation(angle * 180 / M_PI); // cross2d uses degrees.
 
         renderer->flip();
     }
