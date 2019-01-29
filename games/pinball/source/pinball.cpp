cd422c14c7aff404995145a4fa79d051d7266c30
Will Smith <minkcv@gmail.com>
2019-01-29 11:09:02 -0700

    move pinball into launch tube, add restitution for bounciness

--- games/pinball/source/pinball.cpp
+++ games/pinball/source/pinball.cpp
@@ -5,12 +5,13 @@ Pinball::Pinball(C2DRenderer* renderer, b2World& world) {
     circle.m_radius = m_radius;
     b2BodyDef circleBodyDef;
     circleBodyDef.type = b2_dynamicBody;
-    circleBodyDef.position.Set(3.0f, 2.0f);
+    circleBodyDef.position.Set(2.0f, 6.9f);
     m_body = world.CreateBody(&circleBodyDef);
     b2FixtureDef circleFixtureDef;
     circleFixtureDef.shape = &circle;
     circleFixtureDef.density = 1.0f;
     circleFixtureDef.friction = 0.3f;
+    circleFixtureDef.restitution = 0.3;
     m_body->CreateFixture(&circleFixtureDef);
 
     m_shape = new CircleShape(m_radius * g_graphicsScale);
