5550a0321e1dc93054013bd303e12509cc38d40c
Will Smith <minkcv@gmail.com>
2019-01-30 12:56:25 -0700

    fix flipper positions and shapes

--- games/pinball/source/flipper.cpp
+++ games/pinball/source/flipper.cpp
@@ -18,15 +18,19 @@ Flipper::Flipper(C2DRenderer* renderer, b2World& world, bool rightFlipper) {
         m_rotateDirection = 1.0f;
     }
     b2BodyDef bd;
-    bd.position.Set(m_x, m_y);
+    if (rightFlipper)
+        bd.position.Set(m_x - 16 / g_graphicsScale, m_y - 16 / g_graphicsScale);
+    else
+        bd.position.Set(m_x + 16 / g_graphicsScale, m_y + 16 / g_graphicsScale);
     m_pivot = world.CreateBody(&bd);
 
-    b2CircleShape shape;
-    shape.m_radius = 0.1f;
+    b2CircleShape circleShape;
+    circleShape.m_radius = 0.16f;
 
     b2FixtureDef fd;
-    fd.shape = &shape;
+    fd.friction = 0.6f;
     fd.density = 1.0f;
+    fd.shape = &circleShape;
     m_pivot->CreateFixture(&fd);
 
     bd.type = b2_dynamicBody;
@@ -38,7 +42,12 @@ Flipper::Flipper(C2DRenderer* renderer, b2World& world, bool rightFlipper) {
 
     size_t numValues = sizeof(m_points) / sizeof(m_points[0]);
     size_t numPoints = numValues / 2;
-    createFixturesFromPoints(m_body, m_points, numPoints);
+    b2Vec2* vs = getVertexArray(m_points, numPoints);
+    fd.density = 1.0f;
+    b2PolygonShape shape;
+    shape.Set(vs, 8);
+    fd.shape = &shape;
+    m_body->CreateFixture(&fd);
 
     b2RevoluteJointDef jd;
     jd.Initialize(m_pivot, m_body, m_pivot->GetWorldCenter());
@@ -62,19 +71,17 @@ void Flipper::update(unsigned int keys) {
         if (jointAngle * m_rotateDirection < m_lowerLimit)
             m_joint->SetMotorSpeed(0.0f);
         else
-            m_joint->SetMotorSpeed(-10.0f * m_rotateDirection);
+            m_joint->SetMotorSpeed(-15.0f * m_rotateDirection);
     }
     else if (jointAngle * m_rotateDirection < m_upperLimit) {
-        m_joint->SetMotorSpeed(10.0f * m_rotateDirection);
+        m_joint->SetMotorSpeed(15.0f * m_rotateDirection);
     }
     else {
         m_joint->SetMotorSpeed(0.0f);
     }
 
-    Vector2<float> vTopLeft(-16 / g_graphicsScale, -16 / g_graphicsScale);
     b2Vec2 position = m_body->GetPosition();
     float32 angle = m_body->GetAngle();
-    Vector2<float> transform = rotateVector(vTopLeft, -angle);
-    m_cshape->setPosition((position.x + transform.x) * g_graphicsScale, (position.y - transform.y) * g_graphicsScale);
+    m_cshape->setPosition(position.x * g_graphicsScale, position.y * g_graphicsScale);
     m_cshape->setRotation(angle * 180 / M_PI);
 }
