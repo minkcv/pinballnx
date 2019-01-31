7392d2c79dae28737fcfd54edd6abb7045b30221
Will Smith <minkcv@gmail.com>
2019-01-30 19:23:23 -0700

    remove earcut library

--- collisionshapes/earcut/README.md
+++
@@ -1,3 +0,0 @@
-This code originally taken from [https://github.com/mapbox/earcut.hpp](https://github.com/mapbox/earcut.hpp)
-
-License file preserved in [LICENSE.md](./LICENSE.md)
--- collisionshapes/imgtotris.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-FILE=$1
-if [ "$FILE" = "" ]; then
-    echo "usage: ./imgtotris.sh myimage.bmp"
-    exit
-fi
-potrace -a 0 $FILE -b svg -o tmp.svg
-./totris.py 0
-./earcut/main
