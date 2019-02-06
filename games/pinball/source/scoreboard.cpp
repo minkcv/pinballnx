ab79ad41cdc49e3d24e0cd0869df521b6987e32d
Will Smith <minkcv@gmail.com>
2019-02-06 12:28:19 -0700

    fix typo, round corners on gutters

--- games/pinball/source/scoreboard.cpp
+++ games/pinball/source/scoreboard.cpp
@@ -1,7 +1,7 @@
 #include "scoreboard.h"
 
 Scoreboard::Scoreboard(C2DRenderer* renderer) {
-    m_ballsLeftText = new C2DText("Balls: 1/4");
+    m_ballsLeftText = new C2DText("Ball: 1/4");
     m_ballsLeftText->setPosition(1280, 0);
     m_ballsLeftText->setRotation(90);
     renderer->add(m_ballsLeftText);
