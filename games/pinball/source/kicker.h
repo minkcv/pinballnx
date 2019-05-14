74804f29fef29e9541d8e22d823ad009501aab96
Will Smith <minkcv@gmail.com>
2019-05-14 10:35:26 -0600

    narrow gutters

--- games/pinball/source/kicker.h
+++ games/pinball/source/kicker.h
@@ -20,7 +20,7 @@ class Kicker {
         C2DTexture* m_texture;
         b2PrismaticJoint* m_joint;
         float m_leftX = 0.34;
-        float m_leftY = 0.32;
+        float m_leftY = 0.34;
         float m_rightX = 0.34;
         float m_rightY = 6.40;
         float m_halfHeight = 0.1;
