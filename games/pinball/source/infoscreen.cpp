962ce9d8da076ba2135dfcc9ca8310f0990c6ec4
Will Smith <minkcv@gmail.com>
2019-06-29 20:11:55 -0600

    0.6 for switch homebrew app store

--- games/pinball/source/infoscreen.cpp
+++ games/pinball/source/infoscreen.cpp
@@ -11,7 +11,7 @@ InfoScreen::InfoScreen(C2DRenderer* renderer) {
     vector<string> infoText = {
         "PINBALLNX","BY MINKCV", "", "CONTROLS:", 
         "- START / PAUSE", "+ QUIT", "B: NEW GAME", "L / R: FLIPPERS", "STICK DOWN: PLUNGER", "STICK LEFT / RIGHT: TILT", "",
-        "VERSION: 0.5"};
+        "VERSION: 0.6"};
     for (size_t t = 0; t < infoText.size(); t++) {
         C2DText* m_text = new C2DText(infoText.at(t));
         //m_text->setFont(*m_font);
