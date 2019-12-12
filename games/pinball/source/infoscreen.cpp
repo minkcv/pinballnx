76103a4435a0944ab7b71ed6f9712e7ac8e27c1e
Will Smith <minkcv@gmail.com>
2019-12-11 18:47:29 -0700

    make info screen more compact

--- games/pinball/source/infoscreen.cpp
+++ games/pinball/source/infoscreen.cpp
@@ -1,22 +1,21 @@
 #include "infoscreen.h"
 
 InfoScreen::InfoScreen(C2DRenderer* renderer) {
-    m_shape = new C2DRectangle(Vector2f(600, 600));
+    m_shape = new C2DRectangle(Vector2f(500, 600));
     m_shape->setPosition(440, 60);
     m_shape->setLayer(-99);
-    m_shape->setFillColor(Color(25, 25, 25));
+    m_shape->setFillColor(Color::Black);
     renderer->add(m_shape);
     m_controlsImage = new C2DTexture(renderer->getIo()->getDataReadPath() + "pinballnx/controls.png");
-    m_controlsImage->setPosition(500, 80);
+    m_controlsImage->setPosition(480, 80);
     m_controlsImage->setLayer(-99);
     renderer->add(m_controlsImage);
     vector<string> infoText = {
-        "PINBALLNX","BY MINKCV",
-        "VERSION: 0.7"};
+        "PINBALLNX BY MINKCV - VERSION 0.7"};
     for (size_t t = 0; t < infoText.size(); t++) {
         C2DText* m_text = new C2DText(infoText.at(t));
         m_text->setCharacterSize(30);
-        m_text->setPosition(1020 - t * 35, 260);
+        m_text->setPosition(910 - t * 35, 120);
         m_text->setRotation(90);
         renderer->add(m_text);
         m_text->setLayer(-100);
